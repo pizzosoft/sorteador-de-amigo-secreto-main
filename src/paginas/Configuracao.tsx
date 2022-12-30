104f19520b2b37ee2bdd926978be20639057c76f
Raphael Pizzo <raphael.pizzo@pizzosoft.com.br>
2022-12-29 23:56:09 -0300

    Testes da tela principal
    Testes da tela principal

---
+++ src/paginas/Configuracao.tsx
@@ -0,0 +1,13 @@
+import Formuilario from "../componentes/Formulario";
+import ListaParticipantes from "../componentes/ListaParticipantes";
+import Rodape from "../componentes/Rodape";
+
+export default function Configuracao() {
+    return (
+        <>
+            <Formuilario />
+            <ListaParticipantes />
+            <Rodape />
+        </>
+    )
+}
